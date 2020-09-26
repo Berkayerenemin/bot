9eb7f299f9324b3b706ac006d30c285785c77bdf
Berkayeren Emin <birtutamesc@gmail.com>
2020-09-26 12:48:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,13 +12,23 @@ from discord.ext import commands
 bot = commands.Bot(command_prefix='!')
 
 """@bot.event
-async def on_error(ctx, err, *args, **kwargs):
-    if err == "on_command_error":
-        await ctx.channel.send("Bir şeyler yanlış gitti...")
+async def on_error(ctx, err):
+    print(err)
+    if err == "CommandNotFound":
+        await ctx.channel.send("Bir şeyler yanlış gitti...")"""
 
 @bot.event
-async def on_command_error(ctx, exc):
-    await ctx.channel.send("Böyle bir komut hafızamda bulunmuyor!")"""
+async def on_command_error(ctx, exp):
+    await ctx.channel.send("Böyle bir komut hafızamda bulunmuyor!")
+    print(exp)
+
+"""@bot.event
+async def commands.error.CommandNotFound(ctx, exc):
+    await ctx.channel.send("Böyle bişi yok.")
+
+@bot.event
+async def commands.error.MissingRequiredArgument(ctx, exc):
+    await ctx.channel.send("Argüman yok")"""
 
 @bot.event
 async def on_ready():
@@ -38,11 +48,11 @@ import wikipedia
 async def wiki(ctx,*,words):
     await wikiarama.wikipediarama(ctx, words)
 
-@bot.command(aliases=['neredeyim'])
+@bot.command(aliases=['neredeyim', 'kayboldum'])
 async def yardım(ctx):
     await help.yardımkomut(ctx)
     
-@bot.command()
+"""@bot.command()
 @commands.has_permissions(kick_members =True)
 async def sunucudanat(ctx,member : discord.Member, *, reason="Herhangi bir nedeni yok."):
     await help.at(ctx,member,reason="Herhangi bir nedeni yok.")
@@ -50,9 +60,9 @@ async def sunucudanat(ctx,member : discord.Member, *, reason="Herhangi bir neden
 @bot.command()
 @commands.has_permissions(ban_members = True)
 async def banla(ctx,member : discord.Member, *, reason="Herhangi bir nedeni yok"):
-    await help.ban(ctx,member,reason="Herhangi bir nedeni yok")
+    await help.ban(ctx,member,reason="Herhangi bir nedeni yok")"""
 
-@bot.command()
+@bot.command(aliases=['pomodoro'])
 async def p(ctx,iss,dk):
     k = int(dk)
     seconds = k*60
@@ -62,10 +72,10 @@ async def p(ctx,iss,dk):
 async def pt(ctx):
     await pomodoro.zamanlama2(ctx)
 
-@bot.command()
-async def say_hello(ctx):
-    await ctx.channel.send(f"Hello {ctx.author.mention}!")
+@bot.command(aliases=['selam',"naber","ooo","heybot"])
+async def merhaba(ctx):
+    await ctx.channel.send(f"Merhaba {ctx.author.mention}!")
     print(ctx.author.id)
     print(ctx.author.name)
 
-bot.run(to+ke+n)
+bot.run("NTk0MTk0NDIwMDE5MjMyNzg2.XRY4rQ.jHyat6uWxhnsDJI6MHv4UJkbuB4")
